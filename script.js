8199738a96b79eaedd944e77553879bb9eca3abf
Diabuttan <tattoo-horizon.8g@icloud.com>
2025-02-08 02:29:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+
+function nextPage() {
+    window.location.href = 'yes.html';
+}
+
+function moveButton() {
+    const noButton = document.getElementById('noButton');
+    const container = document.querySelector('.container');
+    const containerRect = container.getBoundingClientRect();
+    const buttonRect = noButton.getBoundingClientRect();
+
+    const maxX = containerRect.width - buttonRect.width;
+    const maxY = containerRect.height - buttonRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noButton.style.position = 'absolute';
+    noButton.style.left = `${randomX}px`;
+    noButton.style.top = `${randomY}px`;
+}
